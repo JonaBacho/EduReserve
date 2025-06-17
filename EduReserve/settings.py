1bc53ae9a143fcff08138ea629a7125a1b169ccd
JonaBacho <110006848+JonaBacho@users.noreply.github.com>
2025-06-17 04:52:15 +0100

    Update settings.py

--- EduReserve/settings.py
+++ EduReserve/settings.py
@@ -81,8 +81,7 @@ WSGI_APPLICATION = 'EduReserve.wsgi.application'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
